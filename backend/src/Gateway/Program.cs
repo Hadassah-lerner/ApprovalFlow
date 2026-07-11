1e9500e133de7370499bdede69c74e51a453b4b4
Hadassah Lerner <hadasa.lerner@gmail.com>
2026-07-12 01:59:40 +0300

    feat: finalize Gateway

--- backend/src/Gateway/Program.cs
+++ backend/src/Gateway/Program.cs
@@ -25,10 +25,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseCors("AllowFrontend");
 
 app.MapReverseProxy();
 
-app.Run();
+app.Run("http://localhost:5251");
